2399d1de49b539fbf0c4fa94fd2e4764e6dd189c
Joao Marco <joaomarco.matta@gmail.com>
2022-03-16 21:54:57 -0300

    feat: add context API and change project title

---
+++ client/src/context/AppProvider.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import AppContext from "./AppContext";
+import PropTypes from "prop-types";
+
+export default function AppProvider({ children }) {
+  return (
+    <AppContext.Provider>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
