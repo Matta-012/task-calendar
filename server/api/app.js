d8773748fd805846c4015c7d82f055c1538412d0
Joao Marco <joaomarco.matta@gmail.com>
2022-03-16 22:26:52 -0300

    build: install cors package and use it with express

--- server/api/app.js
+++ server/api/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const cors = require('cors');
 
 const { taskRouter } = require('./routes/routes');
 const { errorHandler } = require('../middlewares');
@@ -9,6 +10,7 @@ const { errorHandler } = require('../middlewares');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/tasks', taskRouter);
 
